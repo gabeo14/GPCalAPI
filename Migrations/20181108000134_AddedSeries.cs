e0477be24a548207477a6405d39c0f39ebdbf8d5
Gabe Oleinik <gomusic14@gmail.com>
2018-11-07 19:04:13 -0500

    Seeded series table

---
+++ Migrations/20181108000134_AddedSeries.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace MotoCalAPI.Migrations
+{
+    public partial class AddedSeries : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Series",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Series", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Series",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { -1, "Formula 1" },
+                    { -2, "Formula E" },
+                    { -3, "World Endurance Championship" },
+                    { -4, "Indy Car" },
+                    { -5, "MotoGP" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Series");
+        }
+    }
+}
---
+++ MotoCalAPIContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using MotoCalAPI.Models;
+
+namespace MotoCalAPI
+{
+  public partial class MotoCalAPIContext : DbContext
+  {
+    public MotoCalAPIContext()
+    {
+    }
+
+    public MotoCalAPIContext(DbContextOptions<MotoCalAPIContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+        optionsBuilder.UseNpgsql("server=localhost;database=MotoCalAPI");
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Series>().HasData(
+          new Series { Id = -1, Name = "Formula 1" },
+          new Series { Id = -2, Name = "Formula E" },
+          new Series { Id = -3, Name = "World Endurance Championship" },
+          new Series { Id = -4, Name = "Indy Car" },
+          new Series { Id = -5, Name = "MotoGP" }
+      );
+    }
+  }
+}
